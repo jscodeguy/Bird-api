d637976ff327b89570210bb27611675b91a3bdcc
NikPurvis <nikpurvis@gmail.com>
2022-04-04 11:13:06 -0600

    Updated picture route, confirmed working with schema

--- Bird-api/server.js
+++ Bird-api/server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const userRoutes = require('./app/routes/user_routes')
 const sightingRoutes = require('./app/routes/sighting_routes')
+const pictureRoutes = require('./app/routes/picture_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -67,6 +68,7 @@ app.use(requestLogger)
 // register route files
 app.use(userRoutes)
 app.use(sightingRoutes)
+app.use(pictureRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
